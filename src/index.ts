3bc8015e74e6b70e4079805152dcb97de5e5a6a4
William Martin <williammartinthird@gmail.com>
2020-02-22 23:07:23 -0700

    Added Sass

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueCompositionApi from '@vue/composition-api';
 import { VNode } from 'vue/types/umd';
 
+import './app.scss';
 import App from './app.vue';
 
 Vue.use(VueCompositionApi);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
@@ -7,6 +8,17 @@ module.exports = {
   mode: 'development',
   module: {
     rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
